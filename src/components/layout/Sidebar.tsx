ab59118282ea66913688d700f6c1ab3152af8d92
didiha <hcring@naver.com>
2025-12-05 07:13:12 +0900

    사이드바 스타일 수정

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Sidebar() {
   return (
     <aside className="fixed left-0 top-0 w-56 h-screen bg-white p-4 flex flex-col flex justify-between">
-      <nav className="flex flex-col gap-3 text-gray-700">
+      <nav className="flex flex-col gap-5 text-gray-700 mt-10">
         <Link href="/dashboard" className="hover:text-black">📊 Dashboard</Link>
         <Link href="/transactions" className="hover:text-black">💳 거래내역</Link>
         <Link href="/merchants" className="hover:text-black">🏬 가맹점 목록</Link>
