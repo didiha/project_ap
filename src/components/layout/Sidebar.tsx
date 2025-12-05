d9118baa9348108254730872034889c4b2384e12
didiha <hcring@naver.com>
2025-12-05 12:07:32 +0900

    수정

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -7,7 +7,7 @@ export default function Sidebar() {
     <aside className="fixed left-0 top-0 w-56 h-screen bg-white p-4 flex flex-col flex justify-between">
       <nav className="flex flex-col gap-5 text-gray-700 mt-10">
         <Link href="/dashboard" className="hover:text-black">📊 Dashboard</Link>
-        <Link href="/transactions" className="hover:text-black">💳 거래내역</Link>
+        <Link href="/payments" className="hover:text-black">💳 거래내역</Link>
         <Link href="/merchants" className="hover:text-black">🏬 가맹점 목록</Link>
       </nav>
 
